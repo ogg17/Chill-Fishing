34cc51230bf92236fc75e9d09425a1cdecee54e3
OGG17 <sewa200018@gmail.com>
2019-08-05 21:52:32 +0300

    0.1.2

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Color pressedColor;
+    public Sprite firstState;
+    public Sprite secondState;
+    public bool changingImage; // enable image change
+    public bool playingSound; // enable playing sound
+
+    public UnityEvent click = new UnityEvent();
+
+    private Image _imageButton;
+    
+    private void Start()
+    {
+        _imageButton = GetComponent<Image>();
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        click.Invoke();
+        if (changingImage) _imageButton.sprite = _imageButton.sprite == firstState ? secondState : firstState;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        _imageButton.color = pressedColor;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        _imageButton.color = Color.white;
+    }
+}
