fcc5c53730bd4b53341b83ffab584a390455c31f
OGG17 <sewa200018@gmail.com>
2019-08-05 20:23:23 +0300

    0.1

---
+++ Assets/Scripts/CommonVariables.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CommonVariables
+{
+    private static bool GamePlaying { get; set; } = false; // Playing game
+}
