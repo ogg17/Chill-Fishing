689e031f2392807a70f2cb31e4c58994adff8cb2
OGG17 <sewa200018@gmail.com>
2019-08-06 15:25:06 +0300

    0.2.3

---
+++ Assets/Scripts/ScoreUIScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUIScript : MonoBehaviour
+{
+    public Sprite firstState;
+    public Sprite secondState;
+    
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeImageState()
+    {
+        if (_spriteRenderer.sprite == firstState) _spriteRenderer.sprite = secondState;
+        else _spriteRenderer.sprite = firstState;
+    }
+}
