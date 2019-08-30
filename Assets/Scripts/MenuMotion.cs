29c33e3ddd1541bc151ffad1afb7b2af1ad90ba0
OGG17 <sewa200018@gmail.com>
2019-08-31 00:54:28 +0300

    0.8.5

---
+++ Assets/Scripts/MenuMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMotion : MonoBehaviour
+{
+    bool EnabledWindow = false;
+    public Vector2 startValue;
+    public Vector2 endValue;
+    public float speed = 0.2f;
+    Vector3 velosity = Vector3.zero;
+    Vector3 moveValue;
+    private void Start()
+    {
+        moveValue = transform.localPosition;
+    }
+    public void MoveWindow()
+    {
+        if (!EnabledWindow) { moveValue.x = endValue.x; moveValue.y = endValue.y; EnabledWindow = true; }
+        else { moveValue.x = startValue.x; moveValue.y = startValue.y; EnabledWindow = false; }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, moveValue, ref velosity, speed);
+    }
+}
